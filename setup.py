5cb319888cad6e64c668b848d6dcf98a9ef6cb44
codecc <abcdef123456chen@sohu.com>
2020-07-13 19:06:49 +0800

    modify color

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ filepath = 'README.md'
 
 setup(
     name='py-log',
-    version="1.8",
+    version="1.9",
     description=(
         'a simple logging tool(support file,email,dingtalk) ,Multithreading and Multi process safe'
     ),
